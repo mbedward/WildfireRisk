05acf1ef45b693fda54c10ee7fec8a86d3c281a8
mbedward <michael.bedward@gmail.com>
2017-09-28 17:06:33 +1000

    Risk functions. Added calculation of fire prob under long-unburnt conditions. Summary function considers both pobs and pmax risk values.

--- R/risk_functions.R
+++ R/risk_functions.R
@@ -4,6 +4,9 @@
 #' and samples time since fire and forest cover values along each line. It then uses
 #' these values to calculate the probability of wildfire travelling along each line.
 #' The calculation is based on the regression equation of Price \emph{et al.} 2015.
+#' The function then calculates a second probability, for comparison, with the time
+#' since fire value for each line set to \code{tsf.max} (a high value representing
+#' long unburnt).
 #'
 #' If the input scan lines have a coordinate reference system, and argument \code{as.sf}
 #' is \code{TRUE} (default), then the same CRS will be set for the output. However,
@@ -26,13 +29,17 @@
 #'
 #' @param kbdi Constant KBDI value to use for calculations (default = 100).
 #'
+#' @param tsfmax High value of time since fire to use for calculation of risk
+#'   if all scan lines are long unburnt (default = 50).
+#'
 #' @param as.sf If \code{TRUE} (default), the returned data frame will be an
 #'   \code{sf} object where the geometry column has the input scan lines.
 #'   If \code{FALSE}, the returned object is a simple data frame with no
 #'   geometry column.
 #'
 #' @return A data frame giving, for each location and scan line, the
-#'   variables used for the risk calculation and the predicted probability.
+#'   variables used for the risk calculation and the predicted probabilities
+#'   for both observed and long-unburnt time since fire (columns 'pobs' and 'pmax').
 #'
 #' @importFrom dplyr %>% do group_by mutate right_join summarize ungroup
 #'
@@ -41,7 +48,7 @@
 calculate_risk <- function(lines,
                            tsf, forest,
                            sample.spacing,
-                           ffdi = 50, kbdi = 100,
+                           ffdi = 50, kbdi = 100, tsfmax = 50,
                            as.sf = TRUE) {
 
   forest <- raster::calc(forest, fun = function(x) x != 0)
@@ -53,6 +60,20 @@ calculate_risk <- function(lines,
   # in the calculations here.
   crs.in <- st_crs(lines)
   st_crs(lines) <- NA_crs_
+
+
+  # Risk function
+  riskfn <- function(tsf_mean, forest_p, distance, is_west) {
+    plogis(58.658 +
+             -9.118 * log(distance) +
+             -40.2 * forest_p +
+             0.041 * kbdi +
+             0.117 * tsf_mean +
+             0.0931 * ffdi +
+             6.4 * log(distance) * forest_p +
+             2.253 * is_west)
+  }
+
 
   dat <- lines %>%
 
@@ -78,19 +99,12 @@ calculate_risk <- function(lines,
     sf::st_sf() %>%
 
     # calculate remaining line variables and probability
-    # of fire travel
+    # of fire travel for observed and maximum time since fire values.
     mutate(distance = sf::st_length(geometry),
-
            is_west = as.integer( is_line_west(geometry) ),
+           pobs = riskfn(tsf_mean, forest_p, distance, is_west),
+           pmax = riskfn(tsfmax, forest_p, distance, is_west) )
 
-           pfire = plogis( 58.658 +
-                             -9.118 * log(distance) +
-                             -40.2 * forest_p +
-                             0.041 * kbdi +
-                             0.117 * tsf_mean +
-                             0.0931 * ffdi +
-                             6.4 * log(distance) * forest_p +
-                             2.253 * is_west ) )
 
   if (!as.sf) {
     dat <- sf::st_sf(dat)
@@ -122,15 +136,22 @@ calculate_risk <- function(lines,
 #' @export
 #'
 summarize_risk <- function(risk) {
+  # Helper function to retrieve central point from a
+  # set of scan lines
   firstpoint <- function(lines) {
     m <- st_coordinates(lines)
     data.frame(x = m[1,1], y = m[1,2])
   }
 
+  # Helper function to calculate quantile
+  q <- function(x, prob) quantile(x, prob, na.rm = TRUE)
+
+  # Get point locations
   loc <- risk %>%
     group_by(locationid) %>%
     do(firstpoint(.$geometry))
 
+  # Summary statistics for each location
   pstats <- risk %>%
     # drop scan lines
     as.data.frame() %>%
@@ -138,10 +159,15 @@ summarize_risk <- function(risk) {
     # calculate mean probabilities
     group_by(locationid) %>%
 
-    summarize(pfire_mean = mean(pfire, na.rm = TRUE),
-              pfire_mid = median(pfire, na.rm = TRUE),
-              pfire_25 = quantile(pfire, 0.25, na.rm = TRUE),
-              pfire_75 = quantile(pfire, 0.75, na.rm = TRUE) ) %>%
+    summarize(pobs_mean = mean(pobs, na.rm = TRUE),
+              pobs_25 = q(pobs, 0.25),
+              pobs_50 = q(pobs, 0.5),
+              pobs_75 = q(pobs, 0.75),
+
+              pmax_mean = mean(pmax, na.rm = TRUE),
+              pmax_25 = q(pmax, 0.25),
+              pmax_50 = q(pmax, 0.5),
+              pmax_75 = q(pmax, 0.75) ) %>%
 
     # join location data
     left_join(loc, by = "locationid") %>%
