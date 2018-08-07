9f84bb2dc0c25263adcd2b4fe390921ed3eb5116
Michael Bedward <michael.bedward@gmail.com>
2018-08-07 10:45:25 +1000

    tidy up package docs

--- R/WildfireRisk.R
+++ R/WildfireRisk.R
@@ -1,6 +1,23 @@
 #' WildfireRisk: Spatial Prediction of Fire Risk
 #'
+#' This package implements the wildfire risk mapping method of Price et al.
+#' (2015). Given a set of locations for which to assess risk; a model relating
+#' fire risk to landscape variables; and a set of raster layers for those
+#' variables; the package provides functions to scan the neighbourhood of each
+#' location and apply the given model to estimate the point risk.
+#'
+#' @section Things to know before using:
+#' Presently the package can only work with models similar to that of Price et
+#' al. (2015).
+#'
+#' @references
+#' Price O, Borah R, Bradstock R, Penman T (2015) An empirical wildfire risk
+#' analysis: the probability of a fire spreading to the urban interface in
+#' Sydney, Australia. International Journal of Wildland Fire 24, 597.
+#' doi:10.1071/WF14160.
+#' \href{http://www.publish.csiro.au/?paper=WF14160}{Article at journal website}
+#'
 #' @docType package
-#' @name BayesFireDanger
+#' @name WildfireRisk
 #'
 NULL
